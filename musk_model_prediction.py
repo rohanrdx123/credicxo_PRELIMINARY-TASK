b0a22b485e4992c4eec4a46ec1eeb6cb14f50309
rohanrdx123 <55513634+rohanrdx123@users.noreply.github.com>
2020-02-19 18:39:28 +0530

    Update musk_model_prediction.py

--- musk_model_prediction.py
+++ musk_model_prediction.py
@@ -17,7 +17,7 @@ y=dataset.iloc[:, 169].values
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
+#PRE PROCEESING OF DATASET
 # Feature  Standard Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
